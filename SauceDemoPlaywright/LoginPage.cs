969e5e87b8e2e5e28a7de01623cb3029937cc710
James Smith <kaleburdev@gmail.com>
2024-09-20 20:59:57 -0400

    Add project files.

---
+++ SauceDemoPlaywright/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace SauceDemoPlaywright
+{
+    public class LoginPage
+    {
+        private readonly IPage _page;
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+            _mainHeading = page.Locator("text=Swag Labs");
+        }
+        public string Url = "https://www.saucedemo.com/";
+
+        private readonly ILocator _mainHeading;
+    }
+}
